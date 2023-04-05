5d63922d2053ec33241a40ade4366b2c43493626
Filipe Bueno <filipe_bueno85@live.com>
2023-04-05 15:14:50 -0300

    Requisito 13 Exibe a relação de pessoas usuárias e favoritadas por faixa etária

--- challenges/desafio13.sql
+++ challenges/desafio13.sql
@@ -4,6 +4,7 @@ WHEN u.age BETWEEN 0 AND 30 THEN 'Até 30 anos'
 WHEN u.age BETWEEN 31 AND 60 THEN 'Entre 31 e 60 anos'
 WHEN u.age > 60 THEN 'Maior de 60 anos'
 ELSE 0
+
 END AS faixa_etaria,
 COUNT(DISTINCT u.user_id) AS total_pessoas_usuarias,
 COUNT(fs.song_id) AS total_favoritadas
